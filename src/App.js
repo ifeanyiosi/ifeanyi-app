9f4da9d02af4913fa6fc46b54ae80e715f1db4d2
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-07-26 14:55:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 // import Sidebar from './Sidebar';
 import axios from 'axios'
+import { data } from 'autoprefixer';
+import { useState } from 'react';
 
 function App() {
+  const [heroes, setHeroes] = useState('')
+
+
   const hero = () => {
     axios
       .get("https://akabab.github.io/superhero-api/api/id/658.json")
       .then((response) => {
         console.log(response);
+        setHeroes(response.data)
       });
   }
   return (
     <div className="">
       <button onClick={hero}>Generate hero</button>
+      <h1>{heroes}</h1>
     </div>
   );
 }

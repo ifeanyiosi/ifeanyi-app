27f9b6428013ec97bc5021ccc3c61b69d8d832a1
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-07-27 00:09:19 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import './App.css';
 // import Sidebar from './Sidebar';
 import axios from 'axios'
 import { data } from 'autoprefixer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import userEvent from '@testing-library/user-event';
 
 function App() {
-  const [heroes, setHeroes] = useState('')
+  const [heroes, setHeroes] = useState([])
 
 
   const hero = () => {
     axios
-      .get("https://akabab.github.io/superhero-api/api/id/658.json")
+      .get("https://akabab.github.io/superhero-api/api/all.json")
       .then((response) => {
         console.log(response);
-        setHeroes(response.data)
+        setHeroes(response.data);
       });
   }
+
+  useEffect(() =>{
+    hero()
+  }, [])
   return (
     <div className="">
-      <button onClick={hero}>Generate hero</button>
-      <h1>{heroes}</h1>
+      {/* <button onClick={hero}>Generate hero</button> */}
+      <ul>
+        {heroes.map(user => (
+          <li key={user.id}>
+            <img src={user.images.xs} alt="" />
+            <h2> {user.name} </h2>
+            
+          </li>
+                 ))
+
+        }
+      </ul>
+
+      {/* <div>
+        <li>
+          {hero.data}
+        </li>
+      </div> */}
     </div>
   );
 }
